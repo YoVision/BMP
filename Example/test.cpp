4be97ee0ec8c34b09854f2ed74df35d521d54026
vLava <j830112@gmail.com>
2015-03-09 23:26:58 +0800

    Example
    放測試程式

---
+++ Example/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "UltraImage.cpp"
+using namespace std;
+
+
+int main()
+{
+    UltraImage clena;
+    clena.Load("pic.bmp");
+    clena.Conv2Gray();
+
+    //clena.Save("pic_test.bmp");
+    
+    system("pause");
+    return 0;
+}
